6e73d15dafd3ba8d6d0fb792e6cb78e1acbafaa0
valseLee <sollleky72@gmail.com>
2022-10-18 21:21:18 +0900

    221018: 과일음료 리스트 뷰 생성

---
+++ SweetSwiftUI/FruitMart/Source/User Interface/Home/FruitListView.swift
@@ -0,0 +1,21 @@
+//
+//  FruitListView.swift
+//  FruitMart
+//
+//  Created by 이승준 on 2022/10/18.
+//  Copyright © 2022 Giftbot. All rights reserved.
+//
+
+import SwiftUI
+
+struct FruitListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitListView()
+    }
+}
