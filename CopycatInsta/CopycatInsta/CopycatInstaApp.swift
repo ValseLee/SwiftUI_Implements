56dc6d59db52962b1fa4f7fb58f5420599cb5510
valseLee <sollleky72@gmail.com>
2022-11-06 19:39:26 +0900

    init: copycat Insta init

---
+++ CopycatInsta/CopycatInsta/CopycatInstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopycatInstaApp.swift
+//  CopycatInsta
+//
+//  Created by 이승준 on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct CopycatInstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
