f788e4943758ae5d0f6d48338759cba22c81bac0
valseLee <sollleky72@gmail.com>
2022-12-04 23:16:27 +0900

    221204: 스포티파이 내 라이브러리 구현 파일 추가

---
+++ PracticingModernUI/PracticingModernUI/MyListView.swift
@@ -0,0 +1,20 @@
+//
+//  MyListView.swift
+//  PracticingModernUI
+//
+//  Created by 이승준 on 2022/12/04.
+//
+
+import SwiftUI
+
+struct MyListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyListView()
+    }
+}
