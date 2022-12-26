8d377b9624a635592f89848f2d22ef2947c6db92
valseLee <sollleky72@gmail.com>
2022-12-26 23:36:37 +0900

    [Chore] 테스트용 코드 작성

--- FirebaseBoilerplate/FirebaseBoilerplate/ContentView.swift
+++ FirebaseBoilerplate/FirebaseBoilerplate/ContentView.swift
@@ -8,19 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+	@EnvironmentObject var firebaseNetworkManager: FirebaseNetworkManager
+	
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+			Text("\(firebaseNetworkManager.viewData)")
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+		.task {
+			await firebaseNetworkManager.fetchMapData()
+			await firebaseNetworkManager.createMapData(with: ItemInfo(option: ItemDetailInfo(amount: 100, color: ["갈색", "파랑"], purchaseData: Date.now)))
+		}
     }
 }
