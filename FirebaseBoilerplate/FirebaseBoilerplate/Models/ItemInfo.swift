4fe8275f7b43c4d59c64e10617ef55761f490e3b
valseLee <sollleky72@gmail.com>
2022-12-26 23:34:56 +0900

    [Feat] Map, Map_[String], Map_Timestamp 타입 프로퍼티를 갖는 구조체 모델 구현

--- FirebaseBoilerplate/FirebaseBoilerplate/Models/ItemInfo.swift
+++ FirebaseBoilerplate/FirebaseBoilerplate/Models/ItemInfo.swift
@@ -5,4 +5,21 @@
 //  Created by 이승준 on 2022/12/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct ItemInfo: Codable, Identifiable {
+	var id = UUID().uuidString
+	var option: ItemDetailInfo
+}
+
+struct ItemDetailInfo: Codable {
+	var amount: Int
+	var color: [String]
+	var purchaseData: Date
+}
+
+enum ItemDetailInfoType {
+	case color(key: String = "color", updateWith: [String])
+	case purchaseDate(key: String = "purchaseDate", updateWith: Date)
+	case amount(key: String = "amount", updateWith: Int)
+}
