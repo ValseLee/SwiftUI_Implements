4c3cc083920c50e5befff17dce24da18e4139632
valseLee <sollleky72@gmail.com>
2022-12-26 21:11:28 +0900

    [init] 파이어베이스 기반코드 아카이브를 위한 프로젝트 파일 생성

---
+++ FirebaseBoilerplate/FirebaseBoilerplate/FirebaseBoilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBoilerplateApp.swift
+//  FirebaseBoilerplate
+//
+//  Created by 이승준 on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBoilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
